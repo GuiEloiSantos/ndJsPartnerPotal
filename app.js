3e2525bb2133edc9e7a91bfec25cc96322d23905
Guilherme Eloi <gcunhaeloI@gmail.com>
2016-11-07 16:18:05 +1100

    Changing the error handling

--- app.js
+++ app.js
@@ -54,23 +54,43 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+    res.status(500);
+
+    // respond with html page
+    if (req.accepts('html')) {
+        res.render('500', { url: req.url, layout:false });
+        return;
+    }
+
+    // respond with json
+    if (req.accepts('json')) {
+        res.send({ error: 'Not found' });
+        return;
+    }
+
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(500);
+
+    // respond with html page
+    if (req.accepts('html')) {
+        res.render('500', { url: req.url, layout:false });
+        return;
+    }
+
+    // respond with json
+    if (req.accepts('json')) {
+        res.send({ error: 'Not found' });
+        return;
+    }
+
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
 });
 
 
